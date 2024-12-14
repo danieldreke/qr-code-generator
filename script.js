ea5c1778b8284d0307c2e0e8f1c3621b0a943c83
Daniel D <22680274+danieldreke@users.noreply.github.com>
2024-12-14 06:43:36 +0100

    Add base files

---
+++ script.js
@@ -0,0 +1,30 @@
+let qrCode;
+
+function updateSize() {
+  // document.getElementById("qr-size-input").value = document.getElementById("qr-size-slider").value;
+  generateQR();
+}
+
+function updateSizeSlider() {
+  document.getElementById("qr-size-slider").value = document.getElementById("qr-size-input").value;
+}
+
+function generateQR() {
+  const content = document.getElementById("qr-content").value;
+  const size = parseInt(document.getElementById("qr-size-slider").value);
+  document.querySelector('#qrcode img')?.remove();
+  document.querySelector('#qrcode canvas')?.remove();
+
+  qrCode = new QRCode("qrcode", {
+    text: content,
+    width: size,
+    height: size
+    // colorDark : "#000000",
+    // colorLight : "#ffffff",
+    // correctLevel : QRCode.CorrectLevel.H
+  });
+}
+
+function deleteContent() {
+  document.getElementById("qr-content").value = '';
+}
